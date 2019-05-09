ef1e54b677dd1df1dfefd9efe216550555a3d9b9
Stuart Clark <stu@rtclark.net>
2019-05-09 16:12:03 +1000

    [#1] Updated Drush integration.

--- scripts/mk/drush.mk
+++ scripts/mk/drush.mk
@@ -3,7 +3,12 @@
 ## Execute a Drush (DRUpal SHell) command.
 drush:
 	@$(call title,Executing Drush command inside php container)
-	@$(call exec,docker-compose exec cli sh -c "drush -r ${DRUSH_ROOT} -l ${DRUSH_URL} $(filter-out $@,$(MAKECMDGOALS))")
+	@$(call drush,$(filter-out $@,$(MAKECMDGOALS)))
+
+## Drush Makefile command.
+define drush
+	@$(call exec,docker-compose exec cli sh -c "drush -r ${DRUSH_ROOT} -l ${DRUSH_URL} ${1}")
+endef
 
 # https://stackoverflow.com/a/6273809/1826109
 %:
